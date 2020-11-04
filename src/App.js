3ed46fcd51096afa5931b124fdb8f0b14d62cb3b
Dmytro Kryvovychev <nondemondias4x@gmail.com>
2020-11-04 11:17:05 +0200

    routes array added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,33 @@ import { Switch, Route, Redirect, Link } from 'react-router-dom';
 
 import { Home, AskNewQuestion, Question } from './pages/index';
 
+const routes = [
+  {
+    name: 'Home',
+    path: '/questions',
+    exact: true,
+    main: () => <Home />,
+  },
+  {
+    name: 'Question',
+    path: '/questions/:id',
+    exact: true,
+    main: () => <Question />,
+  },
+  {
+    name: 'NewQuestion',
+    path: '/new_question',
+    exact: true,
+    main: () => <AskNewQuestion />,
+  },
+  {
+    name: 'ErrorPath',
+    path: '*',
+    exact: true,
+    main: () => <Redirect to="/questions" />,
+  },
+];
+
 function App() {
   return (
     <div>
@@ -9,16 +36,14 @@ function App() {
         <header>Stackoverflow clone</header>
       </Link>
       <Switch>
-        <Route path="/questions" exact>
-          <Home />
-        </Route>
-        <Route path="/questions/:id" exact>
-          <Question />
-        </Route>
-        <Route path="/new_question" exact>
-          <AskNewQuestion />
-        </Route>
-        <Route path="*" exact children={<Redirect to="/questions" />} />
+        {routes.map((route, idx) => (
+          <Route
+            key={`route-${route.name}-idx-${idx}`}
+            path={route.path}
+            exact={route.exact}
+            render={() => <route.main />}
+          />
+        ))}
       </Switch>
     </div>
   );
